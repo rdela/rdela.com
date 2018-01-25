3957318cab17c61bde8d54743e59f3f2f78c370b
Ricky de Laveaga <git@rdela.com>
2018-01-24 23:58:41 -0800

    draft post support and various metadata experimental improvements

--- src/templates/post-article.js
+++ src/templates/post-article.js
@@ -8,11 +8,62 @@ import { rhythm, scale } from '../utils/typography'
 class PostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+    const siteMetadata = get(this.props, 'data.site.siteMetadata')
+    const metaDesc = post.frontmatter.excerpt
+                  ? post.frontmatter.excerpt
+                  : post.excerpt
 
     return (
       <div>
-        <Helmet title={`${post.frontmatter.title}`} />
+        {/* Add long list of social meta tags */}
+
+          <Helmet>
+            <title>{post.frontmatter.title}</title>
+            <link
+              rel="author"
+              href={siteMetadata.siteUrl}
+            />
+            <meta
+              name="description"
+              content={metaDesc}
+            />
+
+            <meta name="og:description" content={metaDesc} />
+            <meta name="twitter:description" content={metaDesc} />
+            <meta name="og:title" content={post.frontmatter.title} />
+            {post.frontmatter.image && (
+              <meta
+                name="og:image"
+                content={`https://gatsbyjs.org${
+                  post.frontmatter.image.childImageSharp.resize.src
+                }`}
+              />
+            )}
+            {post.frontmatter.image && (
+              <meta
+                name="twitter:image"
+                content={`https://gatsbyjs.org${
+                  post.frontmatter.image.childImageSharp.resize.src
+                }`}
+              />
+            )}
+            <meta name="og:type" content="article" />
+            <meta name="article:author" content={siteMetadata.author} />
+            <meta
+              name="twitter:creator"
+              content={siteMetadata.twitter}
+            />
+            <meta name="author" content={siteMetadata.author} />
+            <meta name="twitter:label1" content="Reading time" />
+            <meta
+              name="twitter:data1"
+              content={`${post.timeToRead} min read`}
+            />
+            <meta
+              name="article:published_time"
+              content={post.frontmatter.rawDate}
+            />
+          </Helmet>
         <h1
           style={{
             paddingBottom: rhythm(1 / 32),
@@ -36,17 +87,35 @@ export const pageQuery = graphql`
   query PostBySlug($slug: String!) {
     site {
       siteMetadata {
-        title
         author
+        twitter
       }
     }
     markdownRemark(fields: { slug: { eq: $slug } }) {
       id
       html
+      excerpt
+      timeToRead
+      fields {
+        slug
+      }
       frontmatter {
         title
         date(formatString: "DD MMMM YYYY")
+        excerpt
+        rawDate: date
+        image {
+          childImageSharp {
+            resize(width: 1500, height: 1500) {
+              src
+            }
+            sizes(maxWidth: 786) {
+              ...GatsbyImageSharpSizes
+            }
+          }
+        }
       }
+
     }
   }
 `
