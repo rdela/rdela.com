b332805b38e546f92b86a81c32f9d020011e0a68
Ricky de Laveaga <git@rdela.com>
2018-02-25 08:53:28 -0800

    add 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const NotFoundPage = () => (
+  <div>
+    <h3>Not found</h3>
+    <p>
+      <Link to="/">Take me home!</Link>
+    </p>
+  </div>
+)
+
+export default NotFoundPage
