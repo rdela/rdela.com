7ea23ff048a6009218e06a902c027fcc03c83a36
Ricky de Laveaga <git@rdela.com>
2022-06-09 14:42:52 -0700

    use summary_large_image for twitter cards
    https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/summary-card-with-large-image

--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -28,7 +28,7 @@ class Masthead extends React.Component {
           <meta property="og:image" content={`${siteURL}/rdela-512.png`} />
           <meta property="og:title" content={siteTitle} />
           <meta property="og:description" content={this.props.description} />
-          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content={this.props.siteTwitter} />
           <meta name="twitter:creator" content={this.props.siteTwitter} />
           <meta name="twitter:description" content={this.props.description} />
