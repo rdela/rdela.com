ffb52d83a7c84178cee890fe2dbc8fcb6176deea
Ricky de Laveaga <git@rdela.com>
2022-10-29 16:17:46 -0700

    lose environment console logging

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@
 let activeEnv =
   process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || `development`
 
-console.log(`Using environment config: '${activeEnv}'`)
-console.log(`NODE_ENV is ${process.env.NODE_ENV}`)
+// console.log(`Using environment config: '${activeEnv}'`)
+// console.log(`NODE_ENV is ${process.env.NODE_ENV}`)
 
 require(`dotenv`).config({
   path: `.env.${activeEnv}`,
